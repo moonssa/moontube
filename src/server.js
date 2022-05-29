26a46270a7ac7626443ecf610b3db57dbd50cfec
moonk <mkjoehsb@gmail.com>
2022-05-29 21:32:39 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
+
+const logger = morgan("dev");
+const handleHome = (req, res) => {
+  return res.send("Home Page");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("Login page");
+};
+
+app.use(logger);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`ServerListening on port http://localhost::${PORT}`);
