4d1241f5439761f97a53b7b45afeea918fe6c0e7
moonk <mkjoehsb@gmail.com>
2022-05-30 11:52:57 +0900

    pug template

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,18 @@
+import { fakeUser } from "./videoControllers";
+
 export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
+
+export const login = (req, res) => {
+  fakeUser.loggedIn = true;
+  return res.render("login", { pageTitle: "Login", fakeUser });
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
-export const logout = (req, res) => res.send("Logout");
+
+export const logout = (req, res) => {
+  fakeUser.loggedIn = false;
+  return res.render("logout", { pageTitle: "Logout", fakeUser });
+};
+
 export const see = (req, res) => res.send("See User");
--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,9 +1,42 @@
-export const home = (req, res) => res.send("Home");
+export const fakeUser = {
+  username: "Nick",
+  loggedIn: false,
+};
+
+export const home = (req, res) => {
+  const videos = [
+    {
+      title: "Hello",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 79,
+      id: 1,
+    },
+    {
+      title: "pacinco",
+      rating: 3,
+      comments: 2,
+      createdAt: "3 days ago",
+      views: 1,
+      id: 2,
+    },
+    {
+      title: "path",
+      rating: 2,
+      comments: 5,
+      createdAt: "now",
+      views: 10,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const search = (req, res) => res.send("Search");
 
 export const watch = (req, res) => {
   console.log(req.params);
-  return res.send("Watch Video");
+  return res.render("watch", { pageTitle: "Watch" });
 };
 export const edit = (req, res) => res.send("Edit Video");
 export const deleteVideo = (req, res) => {
